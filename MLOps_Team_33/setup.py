a80feb48bf00bd97179fdef6028f47fa64365030
JPRMEXDI <jose.pena.rodriguez@mabe.com.mx>
2024-10-05 18:34:52 -0700

    Adding template
    Adding template for the project.
    Added a virtual env.
    Added a new jupyter notebook

---
+++ MLOps_Team_33/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Medical entrance examination for admission to medical colleges',
+    author='ITESM-MNA/MLOps_Team_33',
+    license='MIT',
+)
